fd127958b5a4fb96d5f1b9548f63cfc683fe0af6
Harshika-Anand <142821111+Harshika-Anand@users.noreply.github.com>
2023-10-15 17:21:23 +0530

    file.c

--- file.c
+++ file.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main()
+{
+	FILE *fp;
+	char ch;
+	fp=fopen("teach.c","r");   //say teach.c is a sample file
+	while(1)
+	{
+		ch=fgetc(fp);           //accesses each character of the file
+		if(ch==EOF)             //EOF means End Of File
+		break;
+		printf("%c",ch);          //prints all characters in the file
+	}
+	fclose(fp);
+}
